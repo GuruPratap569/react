d7b0a27be84a72cad0db5f7e1f3dcb7dc9592078
Guru Pratap Gupta <155042477+GuruPratap569@users.noreply.github.com>
2024-03-13 19:28:33 +0530

    Update Header.jsx

--- Project/07reactRouter/src/components/Header/Header.jsx
+++ Project/07reactRouter/src/components/Header/Header.jsx
@@ -31,7 +31,8 @@ export default function Header() {
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            {/* <ul className= "flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0" > */}
+            <ul className= "flex flex-row  justify-center font-medium sm:flex-row sm:space-x-3 lg:space-x-8 lg:mt-0" >
               <li>
                 <NavLink
                 to="/"
